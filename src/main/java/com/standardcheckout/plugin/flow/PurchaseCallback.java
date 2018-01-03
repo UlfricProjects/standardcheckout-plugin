51b8817541468752b44fe8c61e345d5734afb013
Adam Harrison <ad@mharrison.us>
2018-01-02 19:38:30 -0500

    PurchaseCallback#success

--- src/main/java/com/standardcheckout/plugin/flow/PurchaseCallback.java
+++ src/main/java/com/standardcheckout/plugin/flow/PurchaseCallback.java
@@ -1,8 +1,28 @@
 package com.standardcheckout.plugin.flow;
 
+import java.util.Objects;
+import java.util.function.Consumer;
+
 import org.bukkit.OfflinePlayer;
 
 public interface PurchaseCallback {
+
+	static PurchaseCallback success(Consumer<OfflinePlayer> callback) {
+		Objects.requireNonNull(callback, "callback");
+
+		return new PurchaseCallback() {
+
+			@Override
+			public void success(OfflinePlayer player) {
+				callback.accept(player);
+			}
+
+			@Override
+			public void failure(OfflinePlayer player) {
+			}
+
+		};
+	}
 
 	void success(OfflinePlayer player);
 
